c65781b34ddee08befaff309bb6181fbbcd32b68
Daniel Rocha <daniel@rocha.im>
2014-01-18 22:10:19 +0100

    Update example.

--- example.c
+++ example.c
@@ -10,10 +10,11 @@
 int main()
 {
     log_set_level(LOG_LEVEL_DEBUG);
-    log_set_format(LOG_PRINT_TIME | LOG_PRINT_TAG | LOG_PRINT_MSG);
+    log_set_format(LOG_PRINT_ALL);
 
     log_debug("Starting.");
     log_trace("Passed here!");
+    log_error("Port %d already in use.", 8080);
     log_fatal("Cannot start server, aborting.");
     log_debug("This should not show.");
 
